4fb91f91ee2cfdbe2f90e35989616fd1e1c29bb3
Greg <gregoryburnett1405@gmail.com>
2021-01-25 11:49:56 -0700

    Added menu links and highlighting

--- gbPort.py
+++ gbPort.py
@@ -8,3 +8,15 @@ app = Flask(__name__)
 @app.route("/about")
 def home():
     return render_template('about.html')
+
+@app.route("/projects")
+def projects():
+    return render_template('projects.html')
+
+@app.route("/timeline")
+def timeline():
+    return render_template('timeline.html')
+
+@app.route("/photography")
+def prhotography():
+    return render_template('photography.html')
